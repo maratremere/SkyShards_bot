f2012fb2f1359af49e1c4ef1aed7b100735be560
Mara <maratremere@gmail.com>
2025-09-10 11:28:55 +0400

    skyshards_bot

--- __init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Основной пакет телеграм-бота
-"""
-#from .bot.handlers import start, stop, help_cmd, get_id
-#from .config.settings import BOT_TOKEN
-
-#__all__ = ['start', 'stop', 'help_cmd', 'get_id', 'BOT_TOKEN']
